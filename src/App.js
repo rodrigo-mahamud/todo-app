2e0956075f7343c5d05230448a177bb7d8894b42
RodrigoMG <rodrigomahamud@gmail.com>
2022-05-31 21:48:43 +0200

    Main table and header created. Checkbox also created and working properly

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { TaskRow } from "./components/taskRow";
+import { TaskBanner } from "./components/taskBanner";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [username, setUserName] = useState("rodri");
+	const [taskItems, setTaskItems] = useState([
+		{
+			name: "task one",
+			done: false,
+		},
+		{
+			name: "task two",
+			done: false,
+		},
+		{
+			name: "task three",
+			done: true,
+		},
+		{
+			name: "task four",
+			done: false,
+		},
+	]);
+	const ToggleTask = (task) => setTaskItems(taskItems.map((t) => (t.name === task.name ? { ...t, done: !t.done } : t)));
+	const taskTableRows = () => taskItems.map((task) => <TaskRow task={task} key={task.name} ToggleTask={ToggleTask} />);
+
+	return (
+		<div>
+			<TaskBanner></TaskBanner>
+			<table className="table table-stripped table-bordered">
+				<thead>
+					<tr>
+						<th>Descripción</th>
+						<th>Hecha</th>
+					</tr>
+				</thead>
+				<tbody>{taskTableRows()}</tbody>
+			</table>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/taskBanner.js
@@ -0,0 +1,5 @@
+import React from "react";
+export const TaskBanner = (props) => 
+<h4 className="bg-primary text-white text-center p-4">
+    ToDo Rodrigo (te quedan X tareas)
+</h4>;
